cba50c0ab6e095f8e28a9bbcfee3089db0302209
d4gl4s <daglas.aitsen@gmail.com>
2023-06-29 18:58:15 +0300

    fixed animation issues

--- app/components/Leffel.tsx
+++ app/components/Leffel.tsx
@@ -1,90 +1,37 @@
 "use client"
 import Link from "next/link"
-import { useRef, useEffect, useState } from "react"
+import { useRef, useEffect } from "react"
 import { gsap } from "gsap"
 
 const TimetableGenerator = () => {
-  const main2 = useRef<HTMLDivElement>(null)
-  const [animationAllowed, setAnimationAllowed] = useState(true)
-
-  const playAnimation = async (time: number) => {
-    animation2.current!.play()
-    gsap.to(animation2.current!, {
-      timeScale: 1,
-      duration: 0,
-      overwrite: true,
-    })
-    await new Promise((resolve: any) => setTimeout(resolve, time)) // 3 sec
-    if (animationAllowed) {
-      gsap.to(animation2.current!, {
-        timeScale: 0,
-        duration: 1.2,
-        overwrite: true,
-        onComplete() {
-          animation2.current!.pause()
-        },
-      })
-    }
-  }
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      playAnimation(4800)
-    }, 20000)
-
-    return () => clearInterval(interval) // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
-  }, [])
-
-  const animation2 = useRef<GSAPTimeline>()
+  const main = useRef<HTMLDivElement>(null)
+  const animation = useRef<GSAPTimeline>()
 
   useEffect(() => {
     let ctx = gsap.context(() => {
       const time = 1.2
 
-      animation2.current = gsap
+      animation.current = gsap
         .timeline({ repeat: -1 })
-
         .to("#s2", { x: -700, ease: "power2.out", duration: time })
         .to("#c4", { x: 300, ease: "power2.out", duration: time }, "-=" + time)
-
         .to("#c1", { y: -200, ease: "power2.out", duration: time })
         .to("#s1", { y: 200, ease: "power2.out", duration: time }, "-=" + time)
-
         .to("#c1", { x: -500, ease: "power2.out", duration: time })
         .to("#c4", { x: 700, ease: "power2.out", duration: time }, "-=" + time)
         .to("#c1", { y: 0, ease: "power2.out", duration: time })
         .to("#s1", { y: 0, ease: "power2.out", duration: time }, "-=" + time)
         .to("#c1", { x: -700, ease: "power2.out", duration: time })
         .to("#s2", { x: 0, ease: "power2.out", duration: time }, "-=" + time)
-
-        .timeScale(0) //Vahalik ainult sujuva peatumise jaoks
-    }, main2)
+      /* .timeScale(0) //Vahalik ainult sujuva peatumise jaoks */
+    }, main)
     return () => ctx.revert()
   }, [])
-  const onHoverEnter = () => {
-    setAnimationAllowed(false)
-    animation2.current!.play()
-    gsap.to(animation2.current!, {
-      timeScale: 1,
-      duration: 0,
-      overwrite: true,
-    })
-  }
-  const onHoverLeave = () => {
-    setAnimationAllowed(true)
-    gsap.to(animation2.current!, {
-      timeScale: 0,
-      duration: 0.2,
-      overwrite: true,
-      onComplete() {
-        animation2.current!.pause()
-      },
-    })
-  }
+
   return (
-    <div className="projects" ref={main2}>
+    <div className="projects" ref={main}>
       <Link href="/leffel">
-        <svg viewBox="0 0 900 600" fill="none" xmlns="http://www.w3.org/2000/svg" onMouseEnter={onHoverEnter} onMouseLeave={onHoverLeave}>
+        <svg viewBox="0 0 900 600" fill="none" xmlns="http://www.w3.org/2000/svg">
           <rect width="900" height="600" fill="#B1A1F1" />
           <rect id="s3" y="400" width="200" height="200" fill="#D9D6FF" />
           <rect id="s2" x="700" width="200" height="200" fill="#6D73FF" />
@@ -95,7 +42,7 @@ const TimetableGenerator = () => {
           <rect id="c3" x="700" y="398" width="200" height="200" rx="100" fill="#6D73FF" />
         </svg>
       </Link>
-      <h2 className="mt-2 2xl:text-[1.1em]" /*  onMouseEnter={onHoverEnter} onMouseLeave={onHoverLeave} */>
+      <h2 className="mt-2 2xl:text-[1.1em]">
         <Link href="/ut-timetable-generator">Original Python game {" 'Leffel'"}</Link>
       </h2>
 
--- app/components/TimetableGenerator.tsx
+++ app/components/TimetableGenerator.tsx
@@ -1,45 +1,10 @@
 "use client"
 import Link from "next/link"
-import { useRef, useEffect, useState } from "react"
+import { useRef, useEffect } from "react"
 import { gsap } from "gsap"
 
 const TimetableGenerator = () => {
   const main = useRef<HTMLDivElement>(null)
-  const [animationAllowed, setAnimationAllowed] = useState(true)
-
-  const delay = async (time: number) => {
-    await new Promise((resolve: any) => setTimeout(resolve, time)) // 3 sec
-    playAnimation(6000) //3 sec
-  }
-
-  const playAnimation = async (time: number) => {
-    animation.current!.play()
-    gsap.to(animation.current!, {
-      timeScale: 1,
-      duration: 0,
-      overwrite: true,
-    })
-    await new Promise((resolve: any) => setTimeout(resolve, time)) // 3 sec
-    if (animationAllowed) {
-      gsap.to(animation.current!, {
-        timeScale: 0,
-        duration: 1.2,
-        overwrite: true,
-        onComplete() {
-          animation.current!.pause()
-        },
-      })
-    }
-  }
-
-  useEffect(() => {
-    delay(3000)
-    const interval = setInterval(() => {
-      playAnimation(4800)
-    }, 20000)
-
-    return () => clearInterval(interval) // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
-  }, [])
 
   const animation = useRef<GSAPTimeline>()
 
@@ -48,7 +13,7 @@ const TimetableGenerator = () => {
       const time = 1.8
       const prefire = "-=" + time
       animation.current = gsap
-        .timeline({ repeat: -1 /* , yoyo: true */ /* , paused: true */ })
+        .timeline({ repeat: -1 })
         .to("#square9", { height: 187.226, y: -100, ease: "power2.out", duration: time })
         .to("#square8", { height: 87.5912, ease: "power2.out", duration: time }, prefire)
 
@@ -66,45 +31,15 @@ const TimetableGenerator = () => {
 
         .to("#square6", { height: 87.5912, y: 0, ease: "power2.out", duration: time }, "-=0.8")
         .to("#square5", { height: 187.226, ease: "power2.out", duration: time }, prefire)
-
-        .timeScale(0) //Vahalik ainult sujuva peatumise jaoks
+      /* .timeScale(0) //Vahalik ainult sujuva peatumise jaoks */
     }, main)
     return () => ctx.revert()
   }, [])
 
-  /* const chooseAnimation = () => {
-    if (tl.current!.reversed()) {
-      tl.current!.play()
-    } else {
-      tl.current!.reverse()
-    }
-  } */
-  const onHoverEnter = () => {
-    setAnimationAllowed(false)
-    animation.current!.play()
-    gsap.to(animation.current!, {
-      timeScale: 1,
-      duration: 0,
-      overwrite: true,
-    })
-  }
-  const onHoverLeave = () => {
-    setAnimationAllowed(true)
-    gsap.to(animation.current!, {
-      timeScale: 0,
-      duration: 0.2,
-      overwrite: true,
-      onComplete() {
-        animation.current!.pause()
-      },
-    })
-
-    /* greenAnimation.current!.pause() */ //nii saab kui pole timescale'i timeline'i juures ning algul on timeline'ile pandud pause  = true
-  }
   return (
     <div className="projects" ref={main}>
       <Link href="/ut-timetable-generator">
-        <svg viewBox="0 0 900 600" fill="none" xmlns="http://www.w3.org/2000/svg" onMouseEnter={onHoverEnter} onMouseLeave={onHoverLeave}>
+        <svg viewBox="0 0 900 600" fill="none" xmlns="http://www.w3.org/2000/svg">
           <rect width="900" height="600" fill="#9ADDAD" />
           <rect id="square2" x="236.496" y="212.409" width="131.387" height="87.5912" rx="31" fill="#4D948F" />
           <rect id="square4" x="384.307" y="112.774" width="131.387" height="87.5912" rx="31" fill="#4D948F" />
@@ -118,7 +53,7 @@ const TimetableGenerator = () => {
           <rect id="square1" x="236.496" y="112.774" width="131.387" height="87.5912" rx="31" fill="#D2FFDE" />
         </svg>
       </Link>
-      <h2 className="mt-2 2xl:text-[1.1em]" /*  onMouseEnter={onHoverEnter} onMouseLeave={onHoverLeave} */>
+      <h2 className="mt-2 2xl:text-[1.1em]">
         <Link href="/ut-timetable-generator">University of Tartu timetable generator</Link>
       </h2>
       <p className="projectYear">2023</p>
